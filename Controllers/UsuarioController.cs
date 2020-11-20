dfcc099c40280f090df68b509db32b6e16b98f92
genkina <cezarcqc@gmail.com>
2020-11-20 10:51:21 -0300

    Add files via upload

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using blueLojas.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace blueLojas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        private static Usuario _usuarios = new Usuario();
+
+        public ActionResult Index()
+        {
+            return View(_usuarios.listaUsuarios);
+        }
+
+        public ActionResult AdicionaUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdicionaUsuario(UsuarioModel _usuarioModel)
+        {
+            _usuarios.CriaUsuario(_usuarioModel);
+            return View();
+        }
+        public ViewResult DeletaUsuario(string id)
+        {
+            return View(_usuarios.GetUsuario(id));
+
+        }
+    }
+}
